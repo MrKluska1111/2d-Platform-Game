90145c050467dbb933e4093b3cde814cb4f2245f
MrKluska11 <kluska1111@gmail.com>
2024-12-27 14:39:59 +0100

    Dodanie poruszania się oraz animacje ruchu i skoku

---
+++ 2d Platform Game/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,57 @@
+using System.Runtime.CompilerServices;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    private Rigidbody2D body;
+    [SerializeField]private float speed;
+    private Animator anim;
+    private bool grounded;
+
+    private void Awake()
+    {
+        body = GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>();
+    }
+
+    private void Update()
+    {
+        float horizontalInput = Input.GetAxis("Horizontal");
+        body.linearVelocity = new Vector2(horizontalInput * speed, body.linearVelocity.y);
+
+        //Flip player when moving left-right
+        if(horizontalInput > 0.01f)
+        {
+            transform.localScale = Vector3.one;
+        }
+        else if(horizontalInput < -0.01f)
+        {
+            transform.localScale = new Vector3(-1, 1, 1);
+        }
+
+        if(Input.GetKey(KeyCode.Space))
+        {
+            Jump();
+        }
+
+        //Set animator parametres
+        anim.SetBool("run", horizontalInput != 0);
+        anim.SetBool("grounded", grounded);
+      
+    }
+
+    private void Jump()
+    {
+        body.linearVelocity = new Vector2(body.linearVelocity.x, speed);
+        anim.SetTrigger("jump");
+        grounded = false;
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if(collision.gameObject.tag == "Ground")
+        {
+            grounded = true;
+        }
+    }
+}
